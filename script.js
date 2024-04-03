d04eac223ff0f3b75a27f677acc147dee52d93a3
Samuel Villers <mathsam68@gmail.com>
2024-04-03 22:00:50 +0200

    Fixed bugs and removed image feature

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ const URL = "https://teachablemachine.withgoogle.com/models/CjH2h8vm9/";
 
 let model, webcam, resultLabel, maxPredictions;
 
+const wordMap = {
+    "trèfle soutterain": "Underground clover",
+    "crépide de nimes": "Nimes crepide",
+    "séneçon du cap": "Cape senecon",
+    "séneçon commun": "Common senecon",
+    "rumex a feuilles obtuses": "Rumex with obtuse leaves",
+    "paquerette": "Daisy",
+    "laurier cerise": "Cherry laurel",
+    "lamier pourpre": "Purple dead-nettle",
+    "jonquille": "Daffodil",
+    "grande oseille": "Sorrel",
+    "géranium a feuilles molles": "Geranium with soft leaves",
+    "fraises": "Strawberries",
+    "crépide tendre": "Tender crepide",
+    "chèvreveuille du japon": "Japanese honeysuckle",
+    "capselle rougeatre": "Reddish shepherd's-purse",
+    "aec-de-grue a feuilles de ciguë": "Crane's-bill with hemlock leaves",
+    "ail des vignes": "Vine garlic",
+};
+
 // Load the image model and setup the webcam
 async function init() {
     const modelURL = URL + "model.json";
@@ -22,11 +42,6 @@ async function init() {
     document.getElementById("overlay").style.display = 'none';
     document.getElementById("startBtn").style.display = "none";
 
-    // Reveal predict and restart button
-    document.querySelectorAll(".appear").forEach(e => {
-        e.style.display = 'block'
-    });
-
     // Convenience function to setup a webcam
     const flip = true; // whether to flip the webcam
     webcam = new tmImage.Webcam(300, 200, flip); // width, height, flip
@@ -37,6 +52,11 @@ async function init() {
     // append elements to the DOM
     document.getElementById("webcam-container").appendChild(webcam.canvas);
     resultLabel = document.getElementById("result");
+
+    // Reveal predict and restart button
+    document.querySelectorAll(".appear").forEach(e => {
+        e.style.display = 'block'
+    });
 }
 
 async function loop() {
@@ -50,17 +70,16 @@ async function predict() {
 
     // predict can take in an image, video or canvas html element
     const prediction = await model.predict(webcam.canvas);
-
     const highestProbabilityPrediction = prediction.reduce((highest, current) => {
         return current.probability > highest.probability ? current : highest;
     }, prediction[0]);
     
     resultLabel.innerHTML = `Nom de la plante : ${highestProbabilityPrediction.className}<br> Probabilité : ${(highestProbabilityPrediction.probability * 100).toFixed(2)}%`;
 
-    generateImageTable(highestProbabilityPrediction.className); // Specify the topic here
-    document.querySelectorAll(".appear2").forEach(e => {
-        e.style.display = 'block'
-    });
+    //generateImageTable(highestProbabilityPrediction.className); // Specify the topic here
+    //document.querySelectorAll(".appear2").forEach(e => {
+        //e.style.display = 'block'
+    //});
 }
 
 async function restart() {
@@ -68,13 +87,10 @@ async function restart() {
 }
 
 async function fetchImages(topic) {
-    const response = await fetch(`https://www.google.com/search?q=${topic}&tbm=isch`);
-    const text = await response.text();
-    const parser = new DOMParser();
-    const htmlDocument = parser.parseFromString(text, 'text/html');
-    const imageElements = htmlDocument.querySelectorAll('img');
-    const imageUrls = Array.from(imageElements).map(img => img.src);
-    return imageUrls;
+    translatedTopic = wordMap[topic.toLowerCase()];
+    const response = await fetch(`https://api.unsplash.com/search/photos?query=${translatedTopic} plant&per_page=16&client_id=R2gELuzY--ILsyt8NXmP3sivjXk8J2r51CbmvC7D6Cg`);
+    const data = await response.json();
+    return data.results.map(result => result.urls.regular);
 }
 
 async function generateImageTable(topic) {
